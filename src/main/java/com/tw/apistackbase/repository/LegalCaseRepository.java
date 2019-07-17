a730d28c19abaaa68d5ad8e23a2015f4c23c9cf9
zllisme <1973842936@qq.com>
2019-07-17 18:56:56 +0800

    feat: find legal case by id

---
+++ src/main/java/com/tw/apistackbase/repository/LegalCaseRepository.java
@@ -0,0 +1,7 @@
+package com.tw.apistackbase.repository;
+
+import com.tw.apistackbase.entity.LegalCase;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LegalCaseRepository extends CrudRepository<LegalCase,Long> {
+}
